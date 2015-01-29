8ab4590f62b68d0d9834c7119104aba6a17a874b
wwq0327 <wwq0327@gmail.com>
2015-01-29 22:26:28 +0800

    if 语句

---
+++ if.swift
@@ -0,0 +1,24 @@
+let num = 19
+
+// 1 
+
+if num == 19 {
+    println("值为19")
+}
+
+// 2
+if num >= 20 {
+    println("大于等于20")
+} else {
+    println("小于20")
+}
+
+
+// 3
+if num >= 20 {
+    println("大于等于20")
+} else if num <= 10 {
+    println("小于等于10")
+} else {
+    println("在10～20之间")
+}
